61d67906c547b78505f0433841b05e4887b3a353
summerbird <8296697+zhang-junzhao@user.noreply.gitee.com>
2023-11-25 17:29:03 +0800

    修改bug

--- deploy.sh
+++ deploy.sh
@@ -44,7 +44,7 @@ echo -e "开始构建 jar 包..."
 
 docker run -it --rm --name todo-maven \
     -v todo-maven-repo:/root/.m2 \
-    -v "$PWD/todo-server":/usr/src/mymaven \
+    -v "$PWD/todo_admin":/usr/src/mymaven \
     -w /usr/src/mymaven \
     maven:3.8.4-jdk-8 mvn clean install package -e '-Dmaven.test.skip=true' || { echo -e "${RED}构建 jar 包失败。${NC}"; exit 1; }
 
